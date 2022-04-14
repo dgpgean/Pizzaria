95efc8212be9cfbd2ab1c8a2854b5d8b574257fa
Diego <dgpgean@gmail.com>
2022-04-14 13:40:52 -0300

    projeto iniciado

---
+++ backend/src/controllers/category/CreateCategoryController.ts
@@ -0,0 +1,16 @@
+import { Response, Request } from "express";
+import { CreateCategoryService } from "../../services/category/CreateCategoryService";
+
+class CreateCategoryController{
+  async handle(req:Request, res:Response){
+    const createCategoryService = new CreateCategoryService()
+
+    const {name} = req.body;
+
+    const category = await createCategoryService.execute({name});
+
+    return res.json(category);
+  }
+}
+
+export {CreateCategoryController}
---
+++ backend/src/services/user/AuthUserService.ts
@@ -0,0 +1,53 @@
+import prismaClient from "../../prisma";
+import {compare} from 'bcryptjs'
+import {sign} from 'jsonwebtoken'
+
+
+interface AuthRequest{
+  email: string;
+  password: string;
+}
+class AuthUserService{
+  async execute({email, password}:AuthRequest){
+    //verificar email
+    const user = await prismaClient.user.findFirst({
+      where:{
+        email
+      }
+    })
+
+    if(!user){
+      throw new Error('User/Password incorret')
+    }
+
+    //verifica senha
+    const passwordMatch = await compare(password,user.password);
+
+    if(!passwordMatch){
+      throw new Error('User/Password incorret')
+    }
+
+    // gera token
+    const token = sign(
+      {
+        name:user.name,
+        email:user.email
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn:'30d'
+      }
+    )
+
+    return {
+      id: user.id,
+      name:user.name,
+      email: user.email,
+      token
+    }
+  }
+}
+
+
+export{AuthUserService}
